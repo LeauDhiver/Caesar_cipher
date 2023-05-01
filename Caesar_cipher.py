3bda494be379b8b687ddf7c0e51682976ff25df0
a31 <a31@a31s-iMac.local>
2023-05-01 17:48:40 +0900

    feat: Create decryption

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -13,3 +13,18 @@ def encrypt_caesar(txt, key):
             result = result + char
 
     return result
+
+## decryption
+def decrypt_caesar(txt, key):
+    result = ""
+
+    for char in txt:
+        if char.isalpha():
+            if char.isupper():
+                result = result + chr((ord(char) - key - 65) % 26 + 65)
+            else:
+                result = result + chr((ord(char) - key - 97) % 26 + 97)
+        else:
+            result = result + char
+
+    return result
